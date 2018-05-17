5c43cb3df1d3121d8f3936bcbe216f49077dfda0
Tomasz Rosiek <tomasz.rosiek@digital.hmrc.gov.uk>
2018-05-17 10:42:00 +0100

    PLATY-502 Added missing files

---
+++ app/util/logging/WithLoggingDetails.scala
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package util.logging
+
+import org.slf4j.MDC
+import uk.gov.hmrc.http.logging.LoggingDetails
+
+object WithLoggingDetails {
+  def withLoggingDetails[T](context: LoggingDetails)(block: => T): T = {
+    val previous = Option(MDC.getCopyOfContextMap)
+    context.mdcData.foreach {
+      case (k, v) => MDC.put(k, v)
+    }
+    try {
+      block
+    } finally {
+      MDC.clear()
+      previous.foreach(MDC.setContextMap)
+    }
+  }
+
+}
---
+++ test/utils/WithLoggingDetailsSpec.scala
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package utils
+
+import org.scalatest.Matchers
+import org.slf4j.MDC
+import uk.gov.hmrc.http.HeaderCarrier
+import uk.gov.hmrc.play.test.UnitSpec
+import util.logging.WithLoggingDetails
+
+class WithLoggingDetailsSpec extends UnitSpec with Matchers {
+  "loggingWithDetails" should {
+    "add details to logging context " in {
+      MDC.put("key1", "old-val")
+      val loggingDetails = new HeaderCarrier() {
+        override lazy val mdcData: Map[String, String] = super.mdcData + ("key1" -> "new-val") + ("key2" -> "some-val")
+      }
+      WithLoggingDetails.withLoggingDetails(loggingDetails) {
+        MDC.get("key1") shouldBe "new-val"
+        MDC.get("key2") shouldBe "some-val"
+      }
+    }
+
+    "restore previous context if it exists" in {
+      MDC.put("key1", "old-val")
+      val loggingDetails = new HeaderCarrier() {
+        override lazy val mdcData: Map[String, String] = super.mdcData + ("key1" -> "new-val") + ("key2" -> "some-val")
+      }
+      WithLoggingDetails.withLoggingDetails(loggingDetails) {
+        //do nothing
+      }
+      MDC.get("key1") shouldBe "old-val"
+      MDC.get("key2") shouldBe null
+
+    }
+
+    "clear context if there was no previous one" in {
+      MDC.clear()
+      val loggingDetails = new HeaderCarrier() {
+        override lazy val mdcData: Map[String, String] = super.mdcData + ("key1" -> "new-val") + ("key2" -> "some-val")
+      }
+      WithLoggingDetails.withLoggingDetails(loggingDetails) {
+        //do nothing
+      }
+      MDC.get("key1") shouldBe null
+      MDC.get("key2") shouldBe null
+    }
+  }
+}

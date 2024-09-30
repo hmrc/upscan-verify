85fe47bc4c4e50b6dd24ebf5ee75b5c0cead8477
colin-lamed <9568290+colin-lamed@users.noreply.github.com>
2024-09-30 10:56:14 +0100

    BDOG-3259 Update dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,11 +2,11 @@ import sbt._
 
 object AppDependencies {
 
-  private val bootstrapVersion = "8.1.0"
+  private val bootstrapVersion = "9.5.0"
 
   private val compile = Seq(
     "uk.gov.hmrc"       %% "bootstrap-backend-play-30"     % bootstrapVersion,
-    "org.typelevel"     %% "cats-core"                     % "2.10.0",
+    "org.typelevel"     %% "cats-core"                     % "2.12.0",
     "com.amazonaws"      % "aws-java-sdk-s3"               % "1.12.606",
     "com.amazonaws"      % "aws-java-sdk-sqs"              % "1.12.606",
     "com.amazonaws"      % "aws-java-sdk-ec2"              % "1.12.606",
